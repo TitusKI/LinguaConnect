a1ee63df96737ddae01e949b6c7bf789b2b6a176
TitusKI <kidumazie@gmail.com>
2025-08-26 09:16:05 +0300

    Add onboarding route and user retrieval endpoint with authentication

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { login, logout, signup } from "../controller/auth.controller.js";
-
+import {
+  login,
+  logout,
+  signup,
+  onboard,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", logout); // post method is used to change the server state
 
+router.post("/onboarding", protectRoute, onboard);
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 export default router;
