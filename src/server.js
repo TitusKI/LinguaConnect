cbb761ab6463831e4313f9a3b4fcb0dafd2181ee
TitusKI <kidumazie@gmail.com>
2025-08-26 09:16:28 +0300

    Add user routes and cookie parser middleware to server configuration

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
+import cookieparser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+// jwt parser
+app.use(cookieparser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
