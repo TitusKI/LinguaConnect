2389ce0d81a27d709863b45d75ce7cc547c10f24
TitusKI <kidumazie@gmail.com>
2025-08-26 11:19:39 +0300

    Add chat routes to server configuration

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 import cookieparser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
@@ -14,6 +15,7 @@ app.use(cookieparser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
